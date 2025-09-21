cc6776704fb638b7beeb98945d1ef0a5deead4ce
NickFlach <flaukowski@proton.me>
2025-09-21 12:34:56 -0500

    refactor: convert config.js from ES modules to CommonJS syntax

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv';
+const dotenv = require('dotenv');
 dotenv.config();
 
 const config = {
@@ -30,4 +30,4 @@ const config = {
     .map(origin => origin.trim()),
 };
 
-export default config;
+module.exports = config;
