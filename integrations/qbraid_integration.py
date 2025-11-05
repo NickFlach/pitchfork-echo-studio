f5335442ff92459192ac064190e70c5621119dda
NickFlach <flaukowski@proton.me>
2025-11-04 20:58:55 -0600

    Quantum Advantage Take 2

--- integrations/qbraid_integration.py
+++ integrations/qbraid_integration.py
@@ -42,9 +42,9 @@ cx q[2], q[3];
 cx q[0], q[2];
 // Apply quantum phase for optimal synthesis
 ry(pi/4) q[0];
-ry(pi/4] q[1];
-ry(pi/8] q[2];
-ry(pi/8] q[3];
+ry(pi/4) q[1];
+ry(pi/8) q[2];
+ry(pi/8) q[3];
 h q[0];
 h q[1];
 // Measure optimal synthesis configuration
@@ -128,9 +128,9 @@ cx q[2], q[3];
 cx q[0], q[3];
 // Apply quantum phase for echo perfection
 ry(pi/4) q[0];
-ry(pi/8] q[1];
-ry(pi/4] q[2];
-ry(pi/8] q[3];
+ry(pi/8) q[1];
+ry(pi/4) q[2];
+ry(pi/8) q[3];
 h q[0];
 h q[1];
 // Measure echo optimization
@@ -216,9 +216,9 @@ cx q[0], q[2];
 cx q[1], q[3];
 // Apply quantum phase for spatial perfection
 ry(pi/4) q[0];
-ry(pi/4] q[1];
-ry(pi/8] q[2];
-ry(pi/8] q[3];
+ry(pi/4) q[1];
+ry(pi/8) q[2];
+ry(pi/8) q[3];
 h q[0];
 h q[1];
 h q[2];
